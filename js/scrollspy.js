81f3a1d6e9b104f7b5fc8280c088479869bc84de
Maxie <erges.maxie@gmail.com>
2017-08-08 19:47:12 +0200

    General Redesign

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -2,18 +2,36 @@
  * All DOM targets...
  */
 var $html = $('html, body'),
-    $nav = $('#menu'),
     $parallaxEl = $('#home .text'),
+    $scrollForMore = $('#home .info'),
     $window = $(window),
-    $indicator = $('#menu .indicator'),
     sectionsSelectors = $('a[href*="#"]').map(function(){
-		var $section = $($(this).attr('href'));
-		if ($section.length && $section.hasClass('section'))
-			return $(this).attr('href');
-	}).toArray(),
-    $sections = $.map(sectionsSelectors, function(selector){
-		return $(selector);
-	});
+			var $section = $($(this).attr('href'));
+			if ($section.length && $section.hasClass('section'))
+				return $(this).attr('href');
+		}).toArray(),
+		$sections = $.map(sectionsSelectors, function(selector){
+			return $(selector);
+		}),
+		$onscrollto = $.map($('[onscrollto]'), function(x){
+			$(x).one('scrolled-to', function(){
+				$(x).addClass($(x).attr('onscrollto'));
+			});
+			return $(x);
+		});
+
+/**
+ * Check if element is visible
+ */
+function isScrolledIntoView(element)
+{
+	var pageTop = $(window).scrollTop();
+	var pageBottom = pageTop + $(window).height();
+	var elementTop = $(element).offset().top;
+	var elementBottom = elementTop + $(element).height();
+
+	return ((pageTop < elementTop) && (pageBottom > elementBottom));
+}
 /**
  * Scroll to section if target exists and have class "section"
  */
@@ -39,22 +57,27 @@ $window.scroll(function()
 	var windowScroll = $window.scrollTop();
 
 	// Parallax effect
-	// $parallaxEl.css('margin-top', (windowScroll / 2.2) + 'px');
-	// if( $window.scrollTop() > 0 ){
-	// 	$nav.removeClass('top');
-	// }else{
-	// 	$nav.addClass('top');
-	// }
+	$parallaxEl.css('margin-top', (windowScroll / 1.7) + 'px');
+	if( $window.scrollTop() - 20 > 0 ){
+		$scrollForMore.addClass('inactive');
+	}else{
+		$scrollForMore.removeClass('inactive');
+	}
+
+	$.map($onscrollto, function(x){
+		if(isScrolledIntoView(x))
+			x.trigger('scrolled-to');
+	});
 
 	// Menu spy for scroll
 	var current = $.map(sectionsSelectors, function(selector){
-		if($(selector).offset().top - 75 < windowScroll)
+		if($(selector).offset().top - 60 < windowScroll)
 			return selector;
 	});
 	current = current[current.length-1];
 	if(lastSelector != current){
-		$('#menu .active').removeClass('active');
-		$('#menu a[href="' + current + '"]').addClass('active');
+		$('.menu .active').removeClass('active');
+		$('.menu a[href="' + current + '"]').addClass('active');
 	}
 });
 /**
